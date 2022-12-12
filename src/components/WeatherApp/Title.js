2b311491a5a18af39c07275b7555a0033d0b724b
fahad-zakir <fahadzakir81@gmail.com>
2022-12-12 00:49:09 -0600

    added-new-files

---
+++ src/components/WeatherApp/Title.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Title() {
+  return (
+    <div className="title">
+      <h1 className="title-container__title">7 day forecast</h1>
+      <h3 className="title-container__subtitle">
+        Lookup seven day forecast by location...
+      </h3>
+    </div>
+  );
+}
+
+export default Title
---
+++ src/components/WeatherApp/Weather.js
@@ -0,0 +1,31 @@
+import React from "react";
+function Weather({ forecast }) {
+  return (
+    <div className="weather-container">
+      {forecast &&
+        forecast.map((day) => {
+          console.log(day);
+          return (
+            <div className="weather-card-container">
+              <div className="ForecastContainer">
+                <div className="icon">
+                  <img
+                    src={day.icon}
+                    alt="weather-icon"
+                    className="weather-icon"
+                  />
+                  <span className="temperature">{day.temperature}℉</span>
+                </div>
+                <div className="day">{day.name}</div>
+                <div className="detail">{day.detailedForecast}</div>
+              </div>
+            </div>
+          );
+        })}
+    </div>
+
+    
+  );
+}
+
+export default Weather;
